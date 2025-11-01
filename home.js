0c6cc00faf87c445441a4349c1dfdcbc1499894d
raising web <183253383+Raising-Web@users.noreply.github.com>
2025-11-01 22:17:33 +0530

    classes part

--- home.js
+++ home.js
@@ -4,6 +4,24 @@ let day = date.getDay();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 
-let currentDate = `${day}/${month}/${year}`
+let currentDate = `${day}/${month}/${year}`;
 
-document.getElementById('date').innerText = currentDate;
+document.getElementById("date").innerText = currentDate;
+
+let classes = [
+  "Pre-Nursary",
+  "Nursary",
+  "KG",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+];
