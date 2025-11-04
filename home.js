624fcbba36e48a9733bd3b918ac5b4048927ccf6
raising web <183253383+Raising-Web@users.noreply.github.com>
2025-11-04 14:04:04 +0530

    4-11

--- home.js
+++ home.js
@@ -8,20 +8,42 @@ let currentDate = `${day}/${month}/${year}`;
 
 document.getElementById("date").innerText = currentDate;
 
-let classes = [
+//
+
+let course = [
   "Pre-Nursary",
-  "Nursary",
+  "Nursery",
   "KG",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
+  "Class 1",
+  "Class 2",
+  "class 3",
+  "Class 4",
+  "Class 5",
+  "Class 6",
+  "Class 7",
+  "Class 8",
+  "Class 9",
+  "Class 10",
+  "Class 11",
+  "Class 12",
+];
+
+course.forEach((value) => {
+  document.getElementById(
+    "courses"
+  ).innerHTML += `<div class="border-0 rounded-xl px-5 py-2 font-semibold bg-blue-500">
+      ${value}
+    </div>`;
+});
+
+//
+
+let testimonial = [
+  {
+    name: "student 1",
+    img: "https://randomuser.me/api/portraits",
+    postion: "top rank in rims",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ut labore et dolore magna aliqua.",
+  },
 ];
